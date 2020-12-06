fb575a4c324c59a2d7e2b587773e88028b8bf118
estein1988 <estein1988@gmail.com>
2020-12-05 17:20:06 -0700

    Complete solution.

--- sum_without_reduce.js
+++ sum_without_reduce.js
@@ -16,7 +16,6 @@ function sumStrings(num1, num2){
     let output = []
     let sum = 0
     let carry = 0
-    let remainder = 0
 
     if(array2.length < array1.length){
         fillArray = new Array(array1.length - array2.length).fill('0')
@@ -30,18 +29,11 @@ function sumStrings(num1, num2){
     for(let i=[array1.length - 1]; i >= 0; i--){
         sum = +array1[i] + +array2[i] + carry
 
-        carry = Math.floor((+array1[i] + +array2[i]) / 10)
+        carry = Math.floor(sum / 10)
         
-        if (+array1[i] + +array2[i] + carry <= 9) {
-            remainder = sum
-        } else if (+array1[i] + +array2[i] + carry === 10) {
-            remainder = '0'
-        } else {
-            remainder = sum % 10
-        }
-        output.push(remainder)
+        output.push(sum % 10)
     }
-    carry > 0 ? output.push(carry) : null
+    if (carry != 0) output.push(carry)
     return output.reverse().join('')
 }
 
@@ -54,21 +46,4 @@ console.log('output:', sumStrings("953", "8475")) // pass
 console.log('output:', sumStrings("8475", "953")) // pass
 console.log('output:', sumStrings("2538", "123")) // pass
 console.log('output:', sumStrings("123", "789")) // pass
-console.log('output:', sumStrings("2578", "123")) // fail - 26101
-
-
-// const sumStrings = (num1, num2) => {
-//     const result = [];
-
-//     let carry = 0;
-
-//     for (let i = num1.length - 1, j = num2.length - 1; i >= 0 || j >= 0 || carry > 0; i--, j--) {
-//         const x = i >= 0 ? num1[i] - '0' : 0;
-//         const y = j >= 0 ? num2[j] - '0' : 0;
-
-//         result.unshift((x + y + carry) % 10);
-//         carry = Math.floor((x + y + carry) / 10);
-//     }
-
-//     return result.join('');
-// };
+console.log('output:', sumStrings("2578", "123")) // pass
